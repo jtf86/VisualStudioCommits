bf1f0699978b65c8c996de7893538a4ea2d727d9
John <jfranti@gmail.com>
2016-03-24 15:43:57 -0700

    Add ninject ctrlFact file

---
+++ src/ToDoList/App_Start/NinjectControllerFactory.cs
@@ -0,0 +1,18 @@
+﻿{
+    public class NinjectControllerFactory : DefaultControllerFactory
+{
+    private IKernel ninjectKernel;
+
+    public NinjectControllerFactory()
+    {
+        ninjectKernel = new StandardKernel();
+        AddBindings();
+    }
+
+    protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+    {
+        return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+    }
+    // My Code Here
+}
+}
