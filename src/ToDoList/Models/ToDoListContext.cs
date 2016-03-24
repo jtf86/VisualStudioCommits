3d2d97068174d48fec85d59edc54e5c885259ea3
John <jfranti@gmail.com>
2016-03-24 12:20:52 -0700

    Remove TestDatabase, add repository for Items

--- src/ToDoList/Models/ToDoListContext.cs
+++ src/ToDoList/Models/ToDoListContext.cs
@@ -13,20 +13,5 @@ namespace ToDoList.Models
         {
             options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoList;integrated security = True");
         }
-
-        public static implicit operator ToDoListContext(TestDatabaseContext v)
-        {
-            throw new NotImplementedException();
-        }
     }
-
-    public class TestDatabaseContext : DbContext
-    {
-        public virtual DbSet<Item> Items { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoListTest;integrated security = True");
-        }
-    }
-
 }
