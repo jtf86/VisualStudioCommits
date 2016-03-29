324955f2b57b883d366a97cf2fcd260c4e2afd6a
John <jfranti@gmail.com>
2016-03-29 11:32:22 -0700

    Ready for publication

--- src/ToDoList/Models/ToDoListContext.cs
+++ src/ToDoList/Models/ToDoListContext.cs
@@ -9,6 +9,7 @@ namespace ToDoList.Models
     public class ToDoListContext : DbContext
     {
         public virtual DbSet<Item> Items { get; set; }
+
         protected override void OnConfiguring (DbContextOptionsBuilder options)
         {
             options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoList;integrated security = True");
