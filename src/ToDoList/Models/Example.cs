6307d7a81e85acde8c54019f3c8fa7622857efc3
John <john@epicodus.com>
2016-04-04 14:51:32 -0700

    update gitignore

--- src/ToDoList/Models/Example.cs
+++ src/ToDoList/Models/Example.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ToDoList;
 using Twilio;
 
 namespace ToDoList.Models
@@ -13,7 +14,7 @@ namespace ToDoList.Models
         {
             // Find your Account Sid and Auth Token at twilio.com/user/account
             string AccountSid = "AC6324afe8d7eefea55b5cdd9fad49e7d0";
-            string AuthToken = "1286dd457e686e5a0b26a5db28684f7c";
+            string AuthToken = (new AuthToken().token);
 
             var twilio = new TwilioRestClient(AccountSid, AuthToken);
             var message = twilio.SendMessage("+15036789613", "+19713134797", item, "");
